2c0c00eb6603ef902352ac77e94b75a529830f23
Hongli Lai (Phusion) <hongli@phusion.nl>
2015-04-01 14:20:53 +0200

    Work in progress

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+MyApp = Flask(__name__)
 
-@app.route("/")
+@MyApp.route("/")
 def hello():
 	return render_template('index.html')
 
 if __name__ == "__main__":
-	app.run()
+	MyApp.run()
